71b09447aac91ab9aca9d8dc871e8959aef9f5a7
Sriram Sundarraj <sriram.s.1994@gmail.com>
2013-08-15 00:13:52 +0530

    Login implemented - basic error messages

--- login.php
+++ login.php
@@ -4,20 +4,40 @@
 	<h1>Please Login</h1>
 	<?php
 		session_start();
+		require "dbconfig.ini";
 		if(isset($_POST['username'])&&isset($_POST['password'])){
-			$valid=0;
-			//validate password
-				$valid=1;
+			//echo $_POST['username'];
+			//echo $_POST['password'];
+			$valid=0; $found=0;
+			$con=mysqli_connect("localhost", $MYDB_USER, $MYDB_PASS,"eventnotif");
+			$sql="SELECT * FROM users";
+			$result=mysqli_query($con, $sql);
+			while($row=mysqli_fetch_array($result)){
+				if($row['username']==$_POST['username']){
+					//$found=1;
+					if($row['password']==$_POST['password']){
+						$valid=1;
+						$uid=$row['uid'];
+					}
+				}
+
+			}
+			mysql_close();
 			if($valid==1){
 				$_SESSION['logged_in']=1;
-				//$_SESSION['userid']=$uid
-				//redirect to index.php
+				$_SESSION['userid']=$uid;
+				echo "Valid credentials";
+				echo "<META HTTP-EQUIV='refresh' content='1; URL=index.php'>";
+			}
+			else{
+				$errormsg="Please enter valid login parameters";
 			}
 		}
 	?>
 	<form method="post" enctype="multipart/form-data" action="login.php">
 	    <table>
 	    <tbody>
+	    <tr><?php echo $errormsg ?></tr>
 	    <tr><th><label for="username">Username</label></th><td>
 	    <input type="text" decsription="Username" name="username" id="username"></td></tr>
 	    <tr><th><label for="password">Password</label></th><td><input type="password" id="password" name="password"></td></tr>
