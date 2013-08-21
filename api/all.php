8bba234f69343fa2c8e2b2aa7cbc4553e6a6c2b6
Sriram Sundarraj <sriram.s.1994@gmail.com>
2013-08-21 18:26:42 +0530

    Require access token to get the data.

--- api/all.php
+++ api/all.php
@@ -1,15 +1,27 @@
 <?php
 	require "../dbconfig.ini";
-	$events=array();
+	$valid=0;
 	$con=mysqli_connect("localhost", $MYDB_USER, $MYDB_PASS,$MYDB_DB);
-	$sql="SELECT * FROM events ORDER BY eid DESC";
-	$result=mysqli_query($con, $sql);
-	while($row=mysqli_fetch_array($result)){
-		$event=array("eid"=> $row['eid'], "uid"=> $row['uid'],"ename"=>$row['ename'], 
-					"edesc"=>$row['edesc'], "edate"=>$row['edate'], "etime"=>$row['etime'],
-					"lat"=>$row['lat'], "lng"=>$row['lng']);
-		array_push($events, $event);
+	$query="SELECT * FROM tokens";
+	$res=mysqli_query($con, $query);
+	while($row=mysqli_fetch_array($res)){
+		if($row['token']==$_POST['token'])
+			$valid=1;
 	}
-	echo json_encode($events);
+	if($valid){
+		$events=array();
+		$sql="SELECT * FROM events ORDER BY eid DESC";
+		$result=mysqli_query($con, $sql);
+		while($row=mysqli_fetch_array($result)){
+			$event=array("eid"=> $row['eid'], "uid"=> $row['uid'],"ename"=>$row['ename'], 
+						"edesc"=>$row['edesc'], "edate"=>$row['edate'], "etime"=>$row['etime'],
+						"lat"=>$row['lat'], "lng"=>$row['lng']);
+			array_push($events, $event);
+		}
+		echo json_encode($events);
+	}
+	else{
+		echo json_encode("err1");
+	}
 	mysql_close($con);
 ?>
